c20991e8fdb4fa68164dd7f81a55d3fb1dd5bc8e
Kelly Chu <kchu@isbx.com>
2021-11-02 11:42:37 -0700

    fix create theme reference

--- packages/react-material-ui/src/theme-provider/createMuiTheme.ts
+++ packages/react-material-ui/src/theme-provider/createMuiTheme.ts
@@ -1,31 +1,37 @@
-import { createMuiTheme as baseCreateMuiTheme } from '@material-ui/core/styles';
+import {
+  createMuiTheme as baseCreateMuiTheme,
+  ThemeOptions as BaseThemeOptions
+} from '@material-ui/core/styles';
 
 export interface StatusLabelColor {
   background?: string;
   color?: string;
 }
 
+export type ThemeOptions = BaseThemeOptions & {
+  pageTemplate?: {
+    headerBackground?: string;
+    headerText?: string;
+    headerLink?: string;
+    headerHoverLink?: string;
+  };
+  cardContent?: {
+    background?: {
+      primary?: string;
+      secondary?: string;
+    };
+  };
+  statusLabel?: {
+    draft?: StatusLabelColor;
+    inProgress?: StatusLabelColor;
+    error?: StatusLabelColor;
+    completed?: StatusLabelColor;
+  };
+};
+
 declare module '@material-ui/core/styles/createMuiTheme' {
-  interface Theme {
-    pageTemplate?: {
-      headerBackground?: string;
-      headerText?: string;
-      headerLink?: string;
-      headerHoverLink?: string;
-    };
-    cardContent?: {
-      background?: {
-        primary?: string;
-        secondary?: string;
-      };
-    };
-    statusLabel?: {
-      draft?: StatusLabelColor;
-      inProgress?: StatusLabelColor;
-      error?: StatusLabelColor;
-      completed?: StatusLabelColor;
-    };
-  }
+  interface Theme extends ThemeOptions {}
+
   // allow configuration using `createMuiTheme`
   interface ThemeOptions {
     pageTemplate?: {
